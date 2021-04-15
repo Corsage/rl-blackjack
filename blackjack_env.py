ab369d84b6c6799d9da6fdf66bb5bb3a942dfed7
Kateryna Shkinova <shkiniova@gmail.com>
2021-04-15 15:53:34 -0400

    added monte-carlo + uses blackjack.py as env

---
+++ blackjack_env.py
@@ -0,0 +1,224 @@
+import random
+import numpy as np
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+INITIAL_BALANCE = 1000
+NUM_DECKS = 6
+import random
+from enum import Enum
+
+# Set to True to see debug messages.
+DEBUGGING = True
+
+class Card:
+    """ A basic card class for Blackjack. """
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.suit + self.rank
+    
+    def get_suit(self):
+        return self.suit
+    
+    def get_rank(self):
+        return self.rank
+
+    def get_value(self):
+        return Blackjack.VALUES[self.rank]
+
+class PlayerType(Enum):
+    """ A self.player can either be a dealer or a person. """
+    DEALER = "DEALER"
+    PERSON = "PERSON"
+
+class Player:
+    """ A basic self.player class for Blackjack. """
+
+    def __init__(self, playerType):
+        self.cards = []
+        self.playerType = playerType
+        self.score = 0
+
+        # Distinction between busting and stopping.
+        self.busted = False
+        self.stop = False
+
+        # For dealers.
+        self.hidden_card = self.playerType == PlayerType.DEALER
+
+    def __str__(self):
+        info = self.playerType.name + " score: " + str(self.score) + "\n"
+        hand = ""
+        for card in self.cards:
+            hand += str(card) + " "
+
+        hand += "SUM: " + str(self.sum_hand())
+
+        return info + hand
+
+    def add_to_score(self, num):
+        self.score += num
+
+    def get_cards(self):
+        return self.cards
+
+    def did_bust(self):
+        return self.busted
+
+    def has_ace(self):
+        for card in self.cards:
+            value = card.get_value()
+            if value == 1:
+                return True
+
+    def sum_hand(self):
+        hand = 0
+        has_ace = False
+
+        for card in self.cards:
+            value = card.get_value()
+
+            if value == 1:
+                has_ace = True
+
+            hand += value
+
+        # Check if ace as 11 helps.
+        if has_ace and (hand + 10) <= 21:
+            return hand + 10
+        
+        return hand
+    
+    def ask(self):
+        if self.playerType == PlayerType.PERSON:
+            temp = str(input("Hit? (Y/N): "))
+
+            if temp == 'Y' or temp == 'y':
+                return True
+
+            self.stand()
+            return False
+
+        elif self.playerType == PlayerType.DEALER:
+            # Dealer must hit in certain conditions.
+            # If the total is 17 or more, it must stand.
+            # If the total is 16 or under, they must take a card.
+            # If the dealer has an ace, and counting it as 11
+            # would bring the total to 17 or more (but not over 21),
+            # the dealer must count the ace as 11 and stand.
+
+            if self.sum_hand() >= 17:
+                self.stand()
+                return False
+            else:
+                return True
+
+    def hit(self, card):
+        self.cards.append(card)
+
+        # Check if busted.
+        if self.sum_hand() > 21:
+            self.busted = True
+
+    def stand(self):
+        self.stop = True
+
+class Blackjack:
+    """ A Blackjack class that emulates the game. """
+
+    SUITS = ('H', 'D', 'C', 'S')
+    RANKINGS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+    VALUES = { 'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10 }
+
+    def __init__(self, decks = 1, rounds = 1):
+        self.nA = 2
+        self.nS = 10*10*2
+        self.rounds = rounds
+        self.decks = decks
+
+        self.reset()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def get_history(self):
+        return self.history
+
+    def deal(self):
+        # Every time we deal a card, it goes into our history.
+        card = self.deck.pop()
+        self.history.append(card)
+        return card
+
+    def distribute_winnings(self):
+        # If both the player and the dealer have a tie—including
+        # with a blackjack—the bet is a tie or “push”.
+        # If both the dealer and player bust, the player loses.
+        # for player in self.players:
+        if not(self.player.did_bust()):
+            if self.dealer.did_bust() or (self.player.sum_hand() > self.dealer.sum_hand()) :
+                reward = 1
+                self.player.add_to_score(1)
+                self.dealer.add_to_score(-1)
+
+            elif self.player.sum_hand() == self.dealer.sum_hand():
+                reward = 0
+                self.player.add_to_score(0)
+                self.dealer.add_to_score(0)
+
+            else:
+                reward = -1
+                self.player.add_to_score(-1)
+                self.dealer.add_to_score(1)
+        else:
+            reward = -1
+            self.player.add_to_score(-1)
+            self.dealer.add_to_score(1)
+        
+        return reward
+
+    def get_obs(self):
+        return (self.player.sum_hand(), self.dealer.get_cards()[0].get_value(), self.player.has_ace())
+    
+    def step(self, a):
+        reward = 0
+        if a==1: # if action == 'hit'
+            self.player.hit(self.deal())
+            done = self.player.did_bust()
+        else:   # action == 'stand'
+            done = True
+            self.player.stand()
+            while self.dealer.sum_hand() < 17:
+                self.dealer.hit(self.deal())
+
+        if done:
+            reward = self.distribute_winnings()
+        return self.get_obs(), reward, done, {}
+
+    def reset(self):
+        
+        self.deck = []
+        self.history = []
+        self.player = Player(PlayerType.PERSON)
+        self.dealer = Player(PlayerType.DEALER)
+
+        # initialize the deck
+        for suit in self.SUITS:
+            for rank in self.RANKINGS:
+                self.deck.append(Card(suit, rank))
+        self.shuffle()
+
+        # deal cards to player and dealer
+        self.player.hit(self.deal())
+        self.player.hit(self.deal())
+
+        self.dealer.hit(self.deal())
+        self.dealer.hit(self.deal())
+
+        return self.get_obs()
+
--- q_learn_blackjack.py
+++ q_learn_blackjack.py
@@ -1,7 +1,8 @@
 import gym
 import numpy as np
+from blackjack_env import *
 
-# import pickle  # -- to save/load trained policy
+import pickle  # -- to save/load trained policy
 
 def state_to_ind(state):
     # State - The observation of a 3-tuple of: 
@@ -10,7 +11,7 @@ def state_to_ind(state):
     #    - and whether or not the player holds a usable ace (0 or 1).   
     # state: [(players_sum),(shown_card),(usable_ace)]
 
-    if state[0]<=11 or state[0]>=21:
+    if state[0]<=11 or state[0]>21:
         return -1
 
     # linear indeces in 3d array shape
@@ -24,17 +25,129 @@ def sample_action(policy, state):
     
     current_sum = state[0]
 
-    nS = 10*10*2
-    nA = 2
-
     # if our sum is <= 11, always hit
     # since we cannot exceed 21
     if current_sum <= 11:
         return 1
 
     # else pick at random
-    all_actions = np.arange(nA)
+    all_actions = np.arange(env.nA)
     return np.random.choice(all_actions, p=policy[state_to_ind(state)])
+
+def take_one_step(env, policy, state):
+    """
+    This function takes one step in the environment according to the stochastic policy.
+
+    Parameters
+    ----------
+        env: given enviroment, here frozenlake
+        policy: np.ndarray[env.nS, env.nA]
+            See the description in `sample_action`.
+        state: int
+            The current state where the agent is in the environment
+
+    Returns
+    -------
+        action: int
+            the action that was chosen from the stochastic policy.
+        reward: float
+            the reward that was obtained during this step
+        new_state: int
+            the new state that the agent transitioned to
+        done: boolean
+            If done is `True` this indicates that we have entered a terminating state
+            (i.e, `new_state` is a terminating state).
+
+    """
+    action = sample_action(policy, state)
+    new_state, reward, done, _ = env.step(action)
+    return action, reward, new_state, done
+
+
+def generate_episode(env, policy, max_steps=500):
+    """
+    Since Monte Carlo methods are based on learning from episodes write a function `random_episode`
+    that generates an episode given the frozenlake environment and a policy.
+
+    Parameters
+    ----------
+        env: given enviroment, here frozenlake
+        policy: np.ndarray[env.nS, env.nA]
+            See the description in `sample_action`.
+        max_steps: int
+            The maximum number of steps that the episode could take. If a terminating state
+            is not reached within this time, terminate the episode.
+
+    Returns
+    -------
+        episode: list of [(state, action, reward)] triplet.
+            For example, [(0,1,0),(4,2,0)] indicates that in the first time
+            we were in state 0 took action 1 and observed reward 0
+            (it also means we transitioned to state 4). Similarly, in the
+            second time step we are in state 4 took action 2 and observed reward 0.
+
+    """
+    episode = []
+    curr_state = env.reset()  # reset the environment and place the agent in the start square
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+
+    steps = 0
+    while True:
+        action, reward, new_state, done = take_one_step(env, policy, curr_state)
+        episode.append((curr_state, action, reward))
+        curr_state = new_state
+        steps+=1
+        if done or steps >= max_steps:
+            break
+        
+    ############################
+    return episode
+
+def generate_returns(episode, gamma=0.9):
+    """
+    Given an episode, generate the total return from each step in the episode based on the
+    discount factor gamma. For example, let the episode be:
+    [(0,1,1),(4,2,-1),(6,3,0),(8,0,2)]
+    and gamma=0.9. Then the total return in the first time step is:
+    1 + 0.9 * -1 + 0.9^2 * 0 + 0.9^3 * 2
+    In the second time step it is:
+    -1 + 0.9 * 0 + 0.9^2 * 2
+    In the third time step it is:
+    0 + 0.9 * 2
+    And finally, in the last time step it is:
+    2
+
+    Parameters
+    ----------
+        episode: list
+            The episode is assumed to be in the same format as the output of the `generate_episode`
+            described above.
+        gamma: float
+            This is the discount factor, which is a number between 0 and 1.
+
+    Returns
+    -------
+        epi_returns: np.ndarray[len(episode)]
+            The array containing the total returns for each step of the episode.
+
+    """
+    len_episode = len(episode)
+    epi_returns = np.zeros(len_episode)
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+    # HINT: Representing immediate reward as a vector and
+    # using a vector of powers of gamma along with `np.dot` will
+    # make this much easier to implement in a few lines of code.
+    # You don't need to use this approach however and use whatever works for you. #
+
+    immediate_return = [x[2] for x in episode]
+    gammas = [gamma**i for i in range(len_episode)]
+    for i in range(len_episode):
+        epi_returns[i] = np.dot(immediate_return[i:], gammas[:len_episode-i])
+
+    ############################
+    return epi_returns
 
 def epsilon_greedy_policy_improve(Q_value, nS, nA, epsilon):
     """Given the Q_value function and epsilon generate a new epsilon-greedy policy.
@@ -43,24 +156,23 @@ def epsilon_greedy_policy_improve(Q_value, nS, nA, epsilon):
 
     Parameters
     ----------
-    Q_value: np.ndarray[nS, nA]
+    Q_value: np.ndarray[env.nS, env.nA]
         Defined similar to the input of `mc_policy_evaluation`.
-    nS: int
+    env.nS: int
         number of states
-    nA: int
+    env.nA: int
         number of actions
     epsilon: float
         current value of epsilon
 
     Returns
     -------
-    new_policy: np.ndarray[nS, nA]
+    new_policy: np.ndarray[env.nS, env.nA]
         The new epsilon-greedy policy according. The shape of the new policy is
         as described in `sample_action`.
     """
 
     new_policy = epsilon * np.ones((nS, nA)) / nA
-
     for s in range(nS):
         max_a = np.argwhere(Q_value[s] == np.amax(Q_value[s])).flatten()
         for a in range(nA):
@@ -71,23 +183,109 @@ def epsilon_greedy_policy_improve(Q_value, nS, nA, epsilon):
 
     return new_policy
 
-def updateQ_policy(s_t1, policy, Q_value, epsilon):
-    a_t1 = sample_action(policy, s_t1)
-    s_t2, r_t1, done, _ = env.step(a_t1)
+# def updateQ_policy(s_t1, policy, Q_value, epsilon):
+#     a_t1 = sample_action(policy, s_t1)
+#     s_t2, r_t1, done, _ = env.step(a_t1)
 
-    if s_t2<=11:
-        return Q_value, policy
+#     if s_t2<=11:
+#         return Q_value, policy
     
-    s_t1_ind = state_to_ind(s_t1)
-    s_t2_ind = state_to_ind(s_t2)
-    Q_value[s_t1_ind, a_t1] += alpha*(r_t1 + gamma*np.max(Q_value[s_t2_ind]) - Q_value[s_t1_ind, a_t1])
-    i += 1
-    epsilon = epsilon/i
-    policy = epsilon_greedy_policy_improve(Q_value, nS, nA, epsilon)
+#     s_t1_ind = state_to_ind(s_t1)
+#     s_t2_ind = state_to_ind(s_t2)
+#     Q_value[s_t1_ind, a_t1] += alpha*(r_t1 + gamma*np.max(Q_value[s_t2_ind]) - Q_value[s_t1_ind, a_t1])
+#     i += 1
+#     epsilon = epsilon/i
+#     policy = epsilon_greedy_policy_improve(Q_value, env.nS, env.nA, epsilon)
 
-    return Q_value, policy 
+#     return Q_value, policy 
 
-def qlearning(env, iterations=1000, gamma=0.9, alpha=0.1):
+
+def mc_policy_evaluation(env, policy, Q_value, n_visits, gamma=0.9):
+    """Update the current Q_values and n_visits by generating one random episode
+    and using the given policy and the Monte Carlo first-visit approach.
+
+    Parameters
+    ----------
+        env: given enviroment, here frozenlake
+        policy: np.ndarray[env.nS, env.nA]
+            See the description in `sample_action`.
+        Q_value: np.ndarray[env.nS, env.nA]
+            The current Q_values. This is a matrix (i.e., 2D array) of size
+            numb_states (env.nS) x numb_actions (env.nA). For example, `Q_value[0, 1]` is the current
+            estimate of the Q_value for state 0 and action 1.
+        n_visits: np.ndarray[env.nS, env.nA]
+            The current number of times a (state, action) pair has been visited.
+            This is a matrix (i.e., 2D array) of size numb_states (env.nS) x numb_actions (env.nA).
+            For example, `n_visits[0, 1]` is the current number of times action 1 has been performed in state 0.
+        gamma: float
+            This is the discount factor, which is a number between 0 and 1.
+    Returns
+    -------
+    value_function: np.ndarray[env.nS]
+        The value function of the given policy, where value_function[s] is
+        the value of state s
+    """
+    episode = generate_episode(env, policy)
+    returns = generate_returns(episode, gamma=gamma)
+    visit_flag = np.zeros((env.nS, env.nA))
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+    for t in range(len(episode)):
+        s, a, r = episode[t]
+        s_ind = state_to_ind(s)
+        if s_ind >= 0:
+            if visit_flag[s_ind,a] == 0:
+                n_visits[s_ind, a] += 1
+                Q_value[s_ind,a] += 1/n_visits[s_ind,a]*(returns[t] - Q_value[s_ind,a])
+                visit_flag[s_ind,a] += 1
+    
+    ############################
+    return Q_value, n_visits
+
+def mc_glie(env, iterations=1000, gamma=0.9, policy=None):
+    """This function implements the first-visit Monte Carlo GLIE policy iteration for finding
+    the optimal policy.
+
+    Parameters
+    ----------
+    env: given enviroment, here frozenlake
+    iterations: int
+        the number of iterations to try
+    gamma: float
+        discount factor
+
+    Returns:
+    ----------
+    Q_value: np.ndarray[env.nS, env.nA]
+        The Q_value at the end of iterations
+    det_policy: np.ndarray[env.nS]
+        The greedy (i.e., deterministic policy)
+    """
+    Q_value = np.zeros((env.nS, env.nA))
+    n_visits = np.zeros((env.nS, env.nA))
+    if policy is None:
+        policy = np.ones((env.nS,env.nA))/env.nA  # initially all actions are equally likely
+    epsilon = 1
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+    i = 0
+    while True:
+        Q_value, n_visits = mc_policy_evaluation(env, policy, Q_value, n_visits)
+        i += 1
+        epsilon = epsilon/i
+        policy = epsilon_greedy_policy_improve(Q_value, env.nS, env.nA, epsilon)
+
+        if i >= iterations:
+            # save policy to continue training from this point
+            with open("mc_policy.pkl", "wb") as output_file:
+                pickle.dump(policy, output_file)
+            break
+
+    ############################
+    det_policy = np.argmax(Q_value, axis=1)
+    return Q_value, det_policy
+
+def qlearning(env, iterations=1000, gamma=0.9, alpha=0.1, policy=None, Q_value=None):
     """This function implements the Q-Learning policy iteration for finding
     the optimal policy.
 
@@ -103,17 +301,17 @@ def qlearning(env, iterations=1000, gamma=0.9, alpha=0.1):
 
     Returns:
     ----------
-    Q_value: np.ndarray[nS, nA]
+    Q_value: np.ndarray[env.nS, env.nA]
         The Q_value at the end of iterations
-    det_policy: np.ndarray[nS]
+    det_policy: np.ndarray[env.nS]
         The greedy (i.e., deterministic policy)
     """
     # states: [(player_sum, shown_card, usable_ace)]
     # [(12-21),(1-10),(True,False)]
-    nS = 10*10*2  # number of states
-    nA = 2  # number of actions: hit or stand
-    Q_value = np.zeros((nS, nA))
-    policy = np.ones((nS,nA))/nA
+    if Q_value is None:
+        Q_value = np.zeros((env.nS, env.nA))
+    if policy is None:
+        policy = np.ones((env.nS,env.nA))/env.nA
     epsilon = 1
     s_t1 = env.reset()  # reset the environment and place the agent in the start square
     s_t1_ind = state_to_ind(s_t1)
@@ -122,15 +320,15 @@ def qlearning(env, iterations=1000, gamma=0.9, alpha=0.1):
     while True:
         a_t1 = sample_action(policy, s_t1)
         s_t2, r_t1, done, _ = env.step(a_t1)
+        # print('sard: ', s_t1, a_t1, r_t1, done)
         if s_t2[0]<=11:
             continue
-        print(s_t1,s_t2)
         s_t2_ind = state_to_ind(s_t2)
         Q_value[s_t1_ind, a_t1] += alpha*(r_t1 + gamma*np.max(Q_value[s_t2_ind]) - Q_value[s_t1_ind, a_t1])
         
         i += 1
         epsilon = epsilon/i
-        policy = epsilon_greedy_policy_improve(Q_value, nS, nA, epsilon)
+        policy = epsilon_greedy_policy_improve(Q_value, env.nS, env.nA, epsilon)
         
         s_t1 = s_t2
         s_t1_ind = s_t2_ind
@@ -143,6 +341,11 @@ def qlearning(env, iterations=1000, gamma=0.9, alpha=0.1):
             s_t1_ind = state_to_ind(s_t1)
      
         if i >= iterations:
+            # save policy to continue training from this point
+            with open("qlearn_policy.pkl", "wb") as output_file:
+                pickle.dump(policy, output_file)
+            with open("qlearn_qvalue.pkl", "wb") as output_file:
+                pickle.dump(Q_value, output_file)
             break
     
     ############################
@@ -157,9 +360,9 @@ def test_performance(env, policy, nb_episodes=500, max_steps=500):
       Parameters
       ----------
       env: gym.core.Environment
-        Environment to play on. Must have nS, nA, and P as
+        Environment to play on. Must have env.nS, env.nA, and P as
         attributes.
-      Policy: np.array of shape [env.nS]
+      Policy: np.array of shape [env.env.nS]
         The action to take at a given state
       nb_episodes: int
         number of episodes to evaluate over
@@ -176,8 +379,8 @@ def test_performance(env, policy, nb_episodes=500, max_steps=500):
         for j in range(max_steps):
             action = 1 if state[0]<=11 else policy[state_to_ind(state)]
             state, reward, done, _ = env.step(action)
-            res_reward+=reward
             if done:
+                res_reward+=reward
                 if reward>0:
                     win+=1
                 elif reward==0:
@@ -185,15 +388,35 @@ def test_performance(env, policy, nb_episodes=500, max_steps=500):
                 else:
                     loss+=1
                 break
-    print("\nSuccess rate over {} episodes:\nwins = {:.2f}%\ndraws = {:.2f}%\nlosses = {:.2f}%\n\n".format(nb_episodes,win/nb_episodes*100,draw/nb_episodes*100,loss/nb_episodes*100))
+    print("""\nSuccess rate over {} episodes:
+    wins = {:.2f}%\ndraws = {:.2f}%\nlosses = {:.2f}%\n
+    Average reward={:.2f}\n"""
+    .format(nb_episodes,win/nb_episodes*100,draw/nb_episodes*100,loss/nb_episodes*100,res_reward/nb_episodes))
 
 
 if __name__ == "__main__":
-    env = gym.make("Blackjack-v0")
-    nS = 10*10*2  # number of states for policy improvement
-    nA = 2  # number of actions: hit or stand
-    Q_value = np.zeros((nS, nA))
-    policy = np.ones((nS,nA))/nA
-    state = env._get_obs()
-    Q_ql, policy_ql = qlearning(env, iterations=10000, gamma=0.9, alpha=0.1)
+    env = Blackjack()
+    # env = gym.make("Blackjack-v0")
+    nS = env.nS  # number of states for policy improvement
+    nA = env.nA  # number of actions: hit or stand
+    Q_value = np.zeros((env.nS, env.nA))
+    policy = np.ones((env.nS,env.nA))/env.nA
+    state = env.get_obs()
+
+    ## Monte-Carlo
+    # with open("mc_policy.pkl", "rb") as input_file:
+    #     policy = pickle.load(input_file) # load saved policy
+    # print(policy)
+
+    # Q_mc, policy_mc = mc_glie(env, iterations=1000, gamma=0.9, policy=policy)
+    # print(policy_mc)
+    # test_performance(env, policy_mc)
+
+    # with open("mc_det_policy.pkl", "wb") as output_file:
+    #     pickle.dump(policy_mc, output_file)
+
+
+    Q_ql, policy_ql = qlearning(env, iterations=1000, gamma=0.9, alpha=0.1)
+    print(policy_ql)
     test_performance(env, policy_ql, nb_episodes=500, max_steps=500)
+    
